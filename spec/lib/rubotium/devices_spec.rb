83c9dc64b318246f686a779167ff3bc44c7333e1
Slawomir Smiechura <slawomir@soundcloud.com>
2014-09-07 21:08:01 +0200

    Test cleanup

--- spec/lib/rubotium/devices_spec.rb
+++ spec/lib/rubotium/devices_spec.rb
@@ -2,49 +2,53 @@ require 'spec_helper'
 require 'ostruct'
 
 describe Rubotium::Devices do
-  let(:devices_class) {described_class}
-  let(:device) {OpenStruct.new(:name=>"TestDevice")}
+  let(:devices_class) { described_class }
+  let(:device) { OpenStruct.new(:name => "TestDevice") }
 
   before do
     Rubotium::Device.stub(:new).and_return(device)
   end
 
-  context 'with matched device' do
-    let(:devices) {devices_class.new(:name=>'TestDevice')}
+  context 'when attached device' do
     before do
       Rubotium::Adb::Devices.any_instance.stub(:attached).and_return([device])
     end
 
-    it 'should return matched devices' do
-      devices.all.should == [ device ]
+    context 'is matched' do
+      let(:devices) { devices_class.new(:name => 'TestDevice') }
+
+      it 'should return matched devices' do
+        devices.all.should == [device]
+      end
     end
 
+    context 'is not matched' do
+      let(:devices) { devices_class.new(:name => 'Nexus') }
+
+      it 'should raise exception about not matched devices' do
+        expect { devices.all }.to raise_error(Rubotium::NoMatchedDevicesError)
+      end
+    end
+
+    context 'without given matcher' do
+      let(:devices) { devices_class.new }
+
+      it 'should return all available devices' do
+        devices.all.should == [device]
+      end
+
+    end
   end
 
-  context 'with no devices attached devices' do
-    let(:devices) { devices_class.new(:name=>'Nexus') }
+  context 'with no devices attached' do
+    let(:devices) { devices_class.new(:name => 'Nexus') }
 
-    it 'should complain about device being not accessible' do
+    before do
       Rubotium::Adb::Devices.any_instance.stub(:attached).and_return([])
+    end
+
+    it 'should raise exception about device being not connected' do
       expect { devices.all }.to raise_error(Rubotium::NoDevicesError)
     end
   end
-  context 'with no matched' do
-    let(:devices) {devices_class.new(:name=>'Nexus')}
-
-    it 'should complain about not matched devices' do
-      Rubotium::Adb::Devices.any_instance.stub(:attached).and_return([device])
-      expect { devices.all }.to raise_error(Rubotium::NoMatchedDevicesError)
-    end
-  end
-
-  context 'without device matcher' do
-    let(:devices) { devices_class.new }
-
-    it 'should use all available devices' do
-      devices.all.should == [device]
-    end
-
-  end
-
 end
